3fd636bcde0c9039db32d1b9271e98305441747d
Ryan Maas <rdmjobs@live.com>
2021-11-09 11:52:16 -0600

    Inject data into PHP view.

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get(
+  '/',
+  function () {
     return view('welcome');
-});
+  }
+);
 
 // Setting up a new route for pizzas: 
-Route::get('/pizzas', function () {
-  return view('pizzas');
-});
+Route::get(
+  '/pizzas', // This is the URL the user hits. 
+  function () {
+    return view( // This is returning a view:
+      'pizzas', // 'pizzas' is the view we're returning.
+      [ // This is the data we're sending with the view. 
+        // Each key is a variable that can be accessed in the view.
+        'type' => 'hawaiian'
+      ] 
+    );
+    // return 'pizzas';
+    // return ['name' => 'veg pizzas', 'base' => 'classic'];
+  }
+);
+
+// Route::get( // Clearly a GET route. 
+//   '/TestingApp/index', // This is the URL the user will hit. 
+//   array('uses' => 'General\TestingAppController@test') // Then this is the file path. 
+// );
