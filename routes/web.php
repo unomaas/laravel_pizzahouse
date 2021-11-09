2dc4511ab09b983eea79f94ebf05c9aff8fa695d
Ryan Maas <rdmjobs@live.com>
2021-11-09 13:09:29 -0600

    Added loop logic.

--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,16 @@ Route::get(
 Route::get(
   '/pizzas', // This is the URL the user hits. 
   function () {
+    // Moving our data array out of the return and into a variable to return:
+    $pizzas = [ // This is the data we're sending with the view. 
+      // Each key is a variable that can be accessed in the view.
+      ['type' => 'hawaiian', 'base' => 'cheesy crust', 'price' => 10],
+      ['type' => 'volcano', 'base' => 'garlic crust', 'price' => 16],
+      ['type' => 'veg supreme', 'base' => 'thin & crispy', 'price' => 3]
+    ];
     return view( // This is returning a view:
       'pizzas', // 'pizzas' is the view we're returning.
-      [ // This is the data we're sending with the view. 
-        // Each key is a variable that can be accessed in the view.
-        'type' => 'hawaiian'
-      ] 
+      ['pizzas' => $pizzas] // This is the data we're sending with. 
     );
     // return 'pizzas';
     // return ['name' => 'veg pizzas', 'base' => 'classic'];
